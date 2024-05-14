53697592fb01ba6d6b521323b30006f1c494fb71
dasmeee <das me>
2024-05-14 15:23:18 +0100

    now can add todo by pressing Enter key

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,15 +1,31 @@
 export default function TodoInput(props) {
     const { handleAddTodos, todoValue, setTodoValue } = props
+
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            handleAddTodos(todoValue)
+            setTodoValue("")
+        }
+    }
+
     return (
         <header>
-            <input value={todoValue} onChange={(e) => {
-                setTodoValue(e.target.value)
-            }}
-                placeholder="Enter todo..."></input>
-            <button onClick={() => {
-                handleAddTodos(todoValue)
-                setTodoValue("")
-            }}>Add Todo</button>
+            <input
+                value={todoValue}
+                onChange={(e) => {
+                    setTodoValue(e.target.value)
+                }}
+                onKeyDown={handleKeyPress}
+                placeholder="Enter todo..."
+            ></input>
+            <button
+                onClick={() => {
+                    handleAddTodos(todoValue)
+                    setTodoValue("")
+                }}
+            >
+                Add Todo
+            </button>
         </header>
     )
 }
